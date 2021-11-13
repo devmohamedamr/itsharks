cc56a47c8d0461a6850be4c770af61f97ae5879e
mohamed amr <dev.mohamedamr@gmail.com>
2021-11-13 16:49:34 +0200

    add

---
+++ index.php
@@ -0,0 +1,122 @@
+<?php
+
+
+// 
+// $name = 'mohamed';
+// $age = 26;
+// $sallary = 10.5;
+// $is_admin = true;
+
+
+// $item = [ "php","mysql" ];
+
+// echo  gettype($item);
+
+
+
+
+// echo 10 < 5;
+// $st = 10<5;
+
+// if($st){
+//     echo "test";   
+// }
+
+$name = "said";
+
+// if($name == "ahmed"):
+//     echo "ahmed";
+// elseif($name == "eslam"):
+//     echo "eslam";
+// elseif($name == "ramy"):
+//     echo "ramy";
+// else:
+//     echo "nothing";
+// endif;
+
+// if($name == "said"):
+//     echo "said";
+// endif;
+
+
+// $o = 92;
+// $l = 40;
+
+// if($o == 90){
+//     echo $l *2;
+// }elseif($o == 80){
+//     echo $l *1;
+// }elseif($o == 92){
+//     echo $l *4;
+// }else{
+//     echo "not found";
+// }
+
+
+
+// $o = 92;
+// $l = 40;
+// switch($o){
+//     case 80:
+//         echo 1*$l;
+//     break; 
+//     case 90:
+//         echo 2*$l;
+//     break;  
+//     case 92:
+//         echo 4*$l;
+//     break;     
+// }
+
+// if(10<5){
+//     echo "success";
+// }else{
+//     echo "error";
+// }
+
+// echo (10<5) ? "success" : "errro";
+
+
+
+// $d = 75;
+
+// if($d < 50){
+//     echo "ساقط";
+// }elseif($d >= 50 && $d <= 60){
+//     echo "ناجح";     
+// }elseif($d>60 && $d <=70){
+//     echo "جيد";
+// }elseif($d>70 && $d<=80){
+//     echo "جيد جدا";
+// }elseif($d>80 && $d <=100){
+//     echo "امتياز";
+// }else{
+//     echo "العب جنب امك";
+// }
+
+// loop 
+
+
+
+
+// for($x=1;$x<10;$x++){
+//     echo $x."<br>";
+// }
+
+
+
+// $x = 10;
+// echo --$x;
+
+
+
+
+// $x = 1;
+// do{
+
+//     echo $x."<br>";
+//     $x++;
+//     echo $x;
+// }while(12<$x);
+
+
